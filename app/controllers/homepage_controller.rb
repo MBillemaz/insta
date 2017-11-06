cc817930919b49bb858de4a9a13229e35d67b41d
Omegadrien <omegadrien@gmail.com>
2017-11-06 10:28:39 +0100

    add first haml image display

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,12 @@
 class HomepageController < ApplicationController
-    
-      def index
-      end
-    
+
+  def index
+    @images = Image.search(params)
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @images.to_json }
+    end
+  end
+
 end
-    
--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,18 @@ class Image < ApplicationRecord
     belongs_to :category
     has_many :tag, :through => :tag_image
     has_many :likes
+
+      THUMBNAIL_URL = "http://www.latorredelsol.com/press/components/com_easyblog/themes/wireframe/images/placeholder-image.png"
+
+      def thumbnail
+        out = picture.url
+        out = THUMBNAIL_URL unless picture.url
+
+        out
+      end
+
+      def self.search(args)
+        Image.where("name LIKE :query", query: "%#{args[:keywords]}%")
+      end
+
 end
