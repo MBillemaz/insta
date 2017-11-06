4fba15cbcb56d1e8295c95bce98a7bb69d518938
Omegadrien <omegadrien@gmail.com>
2017-11-06 14:56:59 +0100

    first functionnal unit test

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,9 @@
 class Image < ApplicationRecord
-    has_many :users, :through => :image_user
-    belongs_to :category
-    has_many :tag, :through => :tag_image
-    has_many :likes
+  has_many :users, :through => :image_user
+  belongs_to :category
+  has_many :tag, :through => :tag_image
+  has_many :likes
+  mount_uploader :picture, PictureUploader
 
     THUMBNAIL_URL = "http://www.latorredelsol.com/press/components/com_easyblog/themes/wireframe/images/placeholder-image.png"
 
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -7,6 +7,7 @@ describe Image do
   end
 
   it "returns the default image url if the image url is invalid" do
+    assert_equal true, true
     assert_equal "http://www.latorredelsol.com/press/components/com_easyblog/themes/wireframe/images/placeholder-image.png", @imageInvalidUrl.thumbnail
   end
 end
