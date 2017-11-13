3b5986677fc9fae7df16cdd73a177843930a0ded
Omegadrien <omegadrien@gmail.com>
2017-11-13 21:00:58 +0100

    update unit tests, fix a bug with tag search

--- app/models/image.rb
+++ app/models/image.rb
@@ -12,25 +12,11 @@ class Image < ApplicationRecord
 
     validates_presence_of :picture
 
-    THUMBNAIL_URL = "http://www.latorredelsol.com/press/components/com_easyblog/themes/wireframe/images/placeholder-image.png"
-
     def thumbnail
       out = picture.url
-      out = THUMBNAIL_URL unless picture.url
+      out = asset_path("placeholder-image.png") unless picture.url
 
       out
-    end
-
-    def self.search(args)
-        out = []
-        search = args[:search] 
-        if search.first == "#"
-            search[0] = ''
-            out = Image.joins(:tag_images).joins(:tags).where(["tags.name LIKE :query", query: "%#{search.capitalize}%"]).order('images.id DESC')
-        else
-            out = Image.joins(:user).where(["users.first_name LIKE :query", query: "%#{search.capitalize}%"]).order('images.id DESC')
-        end
-        return out.limit(6)
     end
 
     def self.filter(args)
@@ -38,21 +24,34 @@ class Image < ApplicationRecord
         if args[:search].present?
             out = search(args)
         elsif args[:category_id].present?
-            out = Image.where(["category_id = ?",args[:category_id]]).order('id DESC')
+            out = Image.where(["category_id = ?", args[:category_id]]).order('id DESC')
         elsif args[:image_id].present?
             out = Image.where(["id = ?",args[:image_id]]).order('id DESC')
         elsif args[:tag_id].present?
-            out = Image.joins(:tag_images).where(["tag_id = ?",args[:tag_id]])    
+            out = Image.joins(:tag_images).where(["tag_id = ?",args[:tag_id]])
         elsif args[:user_id].present?
-            out = Image.where(["user_id = ?",args[:user_id]]).order('id DESC')      
+            out = Image.where(["user_id = ?",args[:user_id]]).order('id DESC')
         else
-            out = Image.all.order('id DESC')    
+            out = Image.all.order('id DESC')
         end
 
         if args[:last_id].present?
             out = out.where("id < ?", args[:last_id]).order('id DESC')
         end
         return out.limit(6)
+    end
+
+private
+    def self.search(args)
+        out = []
+        search = args[:search]
+        if search.first == "#"
+            search[0] = ''
+            out = Image.joins("join tag_images on tag_images.image_id = images.id").joins("join tags on tags.id = tag_images.tag_id").where(["tags.name LIKE :query", query: "%#{search.capitalize}%"]).order('images.id DESC')
+        else
+            out = Image.joins(:user).where(["users.first_name LIKE :query", query: "%#{search.capitalize}%"]).order('images.id DESC')
+        end
+        return out
     end
 
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 describe Image do
 
-  describe "search" do
-    it "should returns first 6 images if no id in args" do
+  describe "filter" do
+    it "should returns 6 order by desc images if no args" do
       out = Image.filter({})
 
-      assert_equal out, Image.all.first(6)
+      assert_equal out, Image.all.order('id DESC').limit(6)
     end
 
-    it "should returns the correct images with category id 1" do
+    it "should returns the correct number of images with category id 1" do
       out = Image.filter({category_id: Category.first.id})
 
       assert_equal 3, out.length
@@ -18,12 +18,35 @@ describe Image do
       end
     end
 
-    it "should returns the correct images with image id 1" do
+    it "should returns the correct number of image with image id 1" do
       out = Image.filter({image_id: 1})
 
       assert_equal 1, out.length
-      assert_equal out[0].id, 1
     end
+  end
+
+  it "should returns the correct number of images with tag_id 5" do
+    out = Image.filter({tag_id: 5})
+
+    assert_equal 2, out.length
+  end
+
+  it "should returns the correct number of images with user_id 3" do
+    out = Image.filter({user_id: 3})
+
+    assert_equal 2, out.length
+  end
+
+  it "should returns the correct images with last_id = 3" do
+    out = Image.filter({last_id: 3})
+
+    assert_equal 2, out.length
+  end
+
+  it "should returns 6 results maximum" do
+    out = Image.filter({tag_id: 1})
+
+    assert_equal 6, out.length
   end
 
 end
